d86480c53daa2ecef917cb97741e7f145c0d897f
Fan <fan1138612367@vip.qq.com>
2022-01-11 11:07:17 +0800

    ViewPager2滑动切换歌曲

--- app/src/main/java/com/example/music/ui/MainActivity.kt
+++ app/src/main/java/com/example/music/ui/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.music.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.RequestManager
+import com.example.music.R
 import com.example.music.adapters.SwipeSongAdapter
 import com.example.music.data.entities.Song
 import com.example.music.databinding.ActivityMainBinding
+import com.example.music.exoplayer.isPlaying
 import com.example.music.other.Status.*
 import com.example.music.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,11 +30,22 @@ class MainActivity : AppCompatActivity() {
 
     private var curPlayingSong: Song? = null
 
+    private var playbackState: PlaybackStateCompat? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.vpSong.adapter = swipeSongAdapter
-
+        binding.vpSong.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (playbackState?.isPlaying == true) {
+                    mainViewModel.playOrToggleSong(swipeSongAdapter.currentList[position])
+                } else {
+                    curPlayingSong = swipeSongAdapter.currentList[position]
+                }
+            }
+        })
         mainViewModel.mediaItems.observe(this) {
             when (it.status) {
                 SUCCESS -> {
@@ -68,6 +84,29 @@ class MainActivity : AppCompatActivity() {
                 if (newItemIndex != -1) {
                     binding.vpSong.currentItem = newItemIndex
                 }
+            }
+        }
+        mainViewModel.playbackState.observe(this) {
+            playbackState = it
+            binding.ivPlayPause.setImageResource(
+                if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+            )
+        }
+        mainViewModel.isConnected.observe(this) {
+            when (it.getContentIfNotHandled()?.status) {
+                ERROR -> Toast.makeText(this, "An unknown error occurred", Toast.LENGTH_LONG).show()
+                else -> Unit
+            }
+        }
+        mainViewModel.networkError.observe(this) {
+            when (it.getContentIfNotHandled()?.status) {
+                ERROR -> Toast.makeText(this, "An unknown error occurred", Toast.LENGTH_LONG).show()
+                else -> Unit
+            }
+        }
+        binding.ivPlayPause.setOnClickListener {
+            curPlayingSong?.let {
+                mainViewModel.playOrToggleSong(it, true)
             }
         }
     }
